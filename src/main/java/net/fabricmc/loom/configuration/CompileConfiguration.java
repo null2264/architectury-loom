9e7c08f7f1342593c3cdbc625f9ee3689a5837d4
modmuss50 <modmuss50@gmail.com>
2021-07-11 01:53:45 +0800

    Always fork the java compiler
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- src/main/java/net/fabricmc/loom/configuration/CompileConfiguration.java
+++ src/main/java/net/fabricmc/loom/configuration/CompileConfiguration.java
@@ -31,6 +31,7 @@ import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.bundling.AbstractArchiveTask;
 import org.gradle.api.tasks.bundling.Jar;
+import org.gradle.api.tasks.compile.JavaCompile;
 import org.gradle.api.tasks.javadoc.Javadoc;
 
 import net.fabricmc.loom.LoomGradleExtension;
@@ -148,6 +149,11 @@ public final class CompileConfiguration {
 
 		Javadoc javadoc = (Javadoc) p.getTasks().getByName(JavaPlugin.JAVADOC_TASK_NAME);
 		javadoc.setClasspath(main.getOutput().plus(main.getCompileClasspath()));
+
+		p.getTasks().withType(JavaCompile.class).configureEach(compile -> {
+			// Fork the java compiler to ensure that it does not keep any files open.
+			compile.getOptions().setFork(true);
+		});
 
 		p.afterEvaluate(project -> {
 			LoomGradleExtension extension = project.getExtensions().getByType(LoomGradleExtension.class);
