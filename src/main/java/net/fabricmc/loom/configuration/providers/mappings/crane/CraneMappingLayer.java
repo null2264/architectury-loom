97c482ea3015a32fb67436a0571a39dcbd110d6e
shedaniel <daniel@shedaniel.me>
2021-07-14 20:19:18 +0800

    Add crane support
    Signed-off-by: shedaniel <daniel@shedaniel.me>

---
+++ src/main/java/net/fabricmc/loom/configuration/providers/mappings/crane/CraneMappingLayer.java
@@ -0,0 +1,146 @@
+/*
+ * This file is part of fabric-loom, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016, 2017, 2018 FabricMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.fabricmc.loom.configuration.providers.mappings.crane;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.Objects;
+
+import dev.architectury.mappingslayers.api.mutable.MutableClassDef;
+import dev.architectury.mappingslayers.api.mutable.MutableFieldDef;
+import dev.architectury.mappingslayers.api.mutable.MutableMethodDef;
+import dev.architectury.mappingslayers.api.mutable.MutableParameterDef;
+import dev.architectury.mappingslayers.api.mutable.MutableTinyTree;
+import dev.architectury.mappingslayers.api.utils.MappingsUtils;
+import org.apache.commons.io.IOUtils;
+
+import net.fabricmc.loom.configuration.providers.mappings.MappingLayer;
+import net.fabricmc.loom.util.FileSystemUtil;
+import net.fabricmc.mappingio.MappedElementKind;
+import net.fabricmc.mappingio.MappingVisitor;
+
+public final class CraneMappingLayer implements MappingLayer {
+	private final File craneJar;
+
+	public CraneMappingLayer(File craneJar) {
+		this.craneJar = craneJar;
+	}
+
+	public File craneJar() {
+		return craneJar;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) return true;
+		if (obj == null || obj.getClass() != this.getClass()) return false;
+		CraneMappingLayer that = (CraneMappingLayer) obj;
+		return Objects.equals(this.craneJar, that.craneJar);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(craneJar);
+	}
+
+	@Override
+	public String toString() {
+		return "CraneMappingLayer["
+				+ "craneJar=" + craneJar + ']';
+	}
+
+	private static final String TINY_FILE_NAME = "crane.tiny";
+
+	@Override
+	public void visit(MappingVisitor visitor) throws IOException {
+		try (FileSystemUtil.FileSystemDelegate fs = FileSystemUtil.getJarFileSystem(craneJar().toPath(), false)) {
+			try (BufferedReader reader = Files.newBufferedReader(fs.get().getPath(TINY_FILE_NAME), StandardCharsets.UTF_8)) {
+				// can't use this, it requires 2 namespaces
+				// Tiny2Reader.read(reader, mappingVisitor);
+				MutableTinyTree tree = MappingsUtils.deserializeFromString(IOUtils.toString(reader));
+
+				do {
+					if (visitor.visitHeader()) {
+						visitor.visitNamespaces(tree.getMetadata().getNamespaces().get(0), Collections.emptyList());
+					}
+
+					if (visitor.visitContent()) {
+						for (MutableClassDef classDef : tree.getClassesMutable()) {
+							if (visitor.visitClass(classDef.getName(0))) {
+								if (!visitor.visitElementContent(MappedElementKind.CLASS)) {
+									return;
+								}
+
+								for (MutableFieldDef fieldDef : classDef.getFieldsMutable()) {
+									if (visitor.visitField(fieldDef.getName(0), fieldDef.getDescriptor(0))) {
+										if (!visitor.visitElementContent(MappedElementKind.FIELD)) {
+											return;
+										}
+
+										if (fieldDef.getComment() != null) {
+											visitor.visitComment(MappedElementKind.FIELD, fieldDef.getComment());
+										}
+									}
+								}
+
+								for (MutableMethodDef methodDef : classDef.getMethodsMutable()) {
+									if (visitor.visitMethod(methodDef.getName(0), methodDef.getDescriptor(0))) {
+										if (!visitor.visitElementContent(MappedElementKind.METHOD)) {
+											return;
+										}
+
+										for (MutableParameterDef parameterDef : methodDef.getParametersMutable()) {
+											if (visitor.visitMethodArg(parameterDef.getLocalVariableIndex(), parameterDef.getLocalVariableIndex(), parameterDef.getName(0))) {
+												if (!visitor.visitElementContent(MappedElementKind.METHOD_ARG)) {
+													return;
+												}
+
+												if (parameterDef.getComment() != null) {
+													visitor.visitComment(MappedElementKind.METHOD_ARG, parameterDef.getComment());
+												}
+											}
+										}
+
+										if (methodDef.getComment() != null) {
+											visitor.visitComment(MappedElementKind.METHOD, methodDef.getComment());
+										}
+									}
+								}
+
+								if (classDef.getComment() != null) {
+									visitor.visitComment(MappedElementKind.FIELD, classDef.getComment());
+								}
+							}
+						}
+					}
+				} while (!visitor.visitEnd());
+			}
+		}
+	}
+}
--- src/main/java/net/fabricmc/loom/configuration/providers/mappings/crane/CraneMappingsSpec.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/mappings/crane/CraneMappingsSpec.java
@@ -22,29 +22,45 @@
  * SOFTWARE.
  */
 
-package net.fabricmc.loom.util;
+package net.fabricmc.loom.configuration.providers.mappings.crane;
 
-import org.gradle.api.Project;
-import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.Dependency;
-import org.gradle.api.file.FileCollection;
+import java.util.Objects;
 
-/**
- * Simplified dependency downloading.
- *
- * @author Juuz
- */
-public final class DependencyDownloader {
-	/**
-	 * Resolves a dependency as well as its transitive dependencies into a {@link FileCollection}.
-	 *
-	 * @param project            the project needing these files
-	 * @param dependencyNotation the dependency notation
-	 * @return the resolved files
-	 */
-	public static FileCollection download(Project project, String dependencyNotation) {
-		Dependency dependency = project.getDependencies().create(dependencyNotation);
-		Configuration config = project.getConfigurations().detachedConfiguration(dependency);
-		return config.fileCollection(dep -> true);
+import net.fabricmc.loom.configuration.providers.mappings.MappingContext;
+import net.fabricmc.loom.configuration.providers.mappings.MappingsSpec;
+
+public final class CraneMappingsSpec implements MappingsSpec<CraneMappingLayer> {
+	private final String mavenNotation;
+
+	public CraneMappingsSpec(String mavenNotation) {
+		this.mavenNotation = mavenNotation;
+	}
+
+	public String mavenNotation() {
+		return mavenNotation;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) return true;
+		if (obj == null || obj.getClass() != this.getClass()) return false;
+		CraneMappingsSpec that = (CraneMappingsSpec) obj;
+		return Objects.equals(this.mavenNotation, that.mavenNotation);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(mavenNotation);
+	}
+
+	@Override
+	public String toString() {
+		return "CraneMappingsSpec["
+				+ "mavenNotation=" + mavenNotation + ']';
+	}
+
+	@Override
+	public CraneMappingLayer createLayer(MappingContext context) {
+		return new CraneMappingLayer(context.mavenFile(mavenNotation()));
 	}
 }
