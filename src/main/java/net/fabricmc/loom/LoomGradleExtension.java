5eafbbceeb35f8be9a21df7494e935a50e20e119
shedaniel <daniel@shedaniel.me>
2021-07-20 17:30:02 +0800

    Give a proper working directory for GradleMappingContext
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- src/main/java/net/fabricmc/loom/LoomGradleExtension.java
+++ src/main/java/net/fabricmc/loom/LoomGradleExtension.java
@@ -73,6 +73,7 @@ import net.fabricmc.loom.configuration.providers.mappings.LayeredMappingSpecBuil
 import net.fabricmc.loom.configuration.providers.mappings.LayeredMappingsDependency;
 import net.fabricmc.loom.configuration.providers.mappings.MappingsProvider;
 import net.fabricmc.loom.configuration.providers.minecraft.MinecraftMappedProvider;
+import net.fabricmc.loom.configuration.providers.minecraft.MinecraftVersionMeta;
 import net.fabricmc.loom.util.ModPlatform;
 import net.fabricmc.loom.util.function.LazyBool;
 
@@ -227,7 +228,10 @@ public class LoomGradleExtension {
 		LayeredMappingSpecBuilder builder = new LayeredMappingSpecBuilder(this);
 		action.execute(builder);
 		LayeredMappingSpec builtSpec = builder.build();
-		return new LayeredMappingsDependency(new GradleMappingContext(project, "layers_" + builtSpec.getVersion().replace("+", "_").replace(".", "_")), builtSpec, builtSpec.getVersion());
+		return new LayeredMappingsDependency(new GradleMappingContext(project, () -> {
+			MinecraftVersionMeta versionInfo = getMinecraftProvider().getVersionInfo();
+			return "layers/" + versionInfo.getId() + "_" + builtSpec.getVersion().replace("+", "_").replace(".", "_");
+		}), builtSpec, builtSpec.getVersion());
 	}
 
 	public LoomGradleExtension(Project project) {
