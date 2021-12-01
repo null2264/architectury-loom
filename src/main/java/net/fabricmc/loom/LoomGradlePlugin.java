45279bf40822ec5579fd3da75de6a36d1de195e2
shedaniel <daniel@shedaniel.me>
2021-12-02 02:14:03 +0800

    Add warning message for outdated loom
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- src/main/java/net/fabricmc/loom/LoomGradlePlugin.java
+++ src/main/java/net/fabricmc/loom/LoomGradlePlugin.java
@@ -55,6 +55,7 @@ public class LoomGradlePlugin implements Plugin<Project> {
 			loggedVersions.add(loomVersion);
 			System.setProperty("loom.printed.logged", String.join(",", loggedVersions));
 			project.getLogger().lifecycle("Architectury Loom: " + loomVersion);
+			project.getLogger().lifecycle("You are using an outdated version of Architectury Loom! This version will not receive any support, please consider updating!");
 		}
 
 		refreshDeps = project.getGradle().getStartParameter().isRefreshDependencies();
