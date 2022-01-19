161921b0fd3896e049a705d13df302663ac6ba3e
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-01-19 23:32:20 +0200

    Checkstyle

--- src/main/java/net/fabricmc/loom/configuration/providers/forge/ForgeUserdevProvider.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/forge/ForgeUserdevProvider.java
@@ -38,7 +38,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
