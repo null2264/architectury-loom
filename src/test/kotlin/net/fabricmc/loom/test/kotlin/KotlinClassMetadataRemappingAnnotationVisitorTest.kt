a70f45d35980d4d1b38dcfb1f0dedc6dc53b13c8
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-01-19 17:25:03 +0200

    Thank you spotless

--- src/test/kotlin/net/fabricmc/loom/test/kotlin/KotlinClassMetadataRemappingAnnotationVisitorTest.kt
+++ src/test/kotlin/net/fabricmc/loom/test/kotlin/KotlinClassMetadataRemappingAnnotationVisitorTest.kt
@@ -24,12 +24,12 @@
 
 package net.fabricmc.loom.test.kotlin
 
+import dev.architectury.tinyremapper.IMappingProvider
+import dev.architectury.tinyremapper.TinyRemapper
 import java.io.File
 import java.io.PrintWriter
 import java.io.StringWriter
 import java.nio.file.Paths
-import dev.architectury.tinyremapper.IMappingProvider
-import dev.architectury.tinyremapper.TinyRemapper
 import net.fabricmc.loom.kotlin.remapping.KotlinMetadataRemappingClassVisitor
 import net.fabricmc.loom.util.TinyRemapperHelper
 import net.fabricmc.mappingio.MappingReader
