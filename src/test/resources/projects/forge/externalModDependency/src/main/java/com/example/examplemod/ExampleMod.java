40b5fdceadd9f53de80a1a6ef11a841fb73acf95
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-01-27 16:11:31 +0200

    Add test for mod dependencies on Forge

---
+++ src/test/resources/projects/forge/externalModDependency/src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,23 @@
+package com.example.examplemod;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("examplemod")
+public class ExampleMod {
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public ExampleMod() {
+
+        // This is our mod's event bus, used for things like registry or lifecycle events
+        IEventBus MOD_BUS = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // For more information on how to deal with events in Forge,
+        // like automatically subscribing an entire class to an event bus
+        // or using static methods to listen to events,
+        // feel free to check out the Forge wiki!
+    }
+}
