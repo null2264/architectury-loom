c78607ad9728e33750e0778e05cda5a0cef3c4e7
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-04-24 11:24:12 +0300

    Run spotless

--- src/test/groovy/net/fabricmc/loom/test/integration/forge/FmlLogConfigTest.groovy
+++ src/test/groovy/net/fabricmc/loom/test/integration/forge/FmlLogConfigTest.groovy
@@ -1,37 +1,62 @@
+/*
+ * This file is part of fabric-loom, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2023 FabricMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package net.fabricmc.loom.test.integration.forge
 
-import net.fabricmc.loom.test.util.GradleProjectTestTrait
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import net.fabricmc.loom.test.util.GradleProjectTestTrait
 
 import static net.fabricmc.loom.test.LoomTestConstants.DEFAULT_GRADLE
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 class FmlLogConfigTest extends Specification implements GradleProjectTestTrait {
-    @Unroll
-    def "use fml logger config (minecraft #minecraft, forge #forge)"() {
-        setup:
-        def gradle = gradleProject(project: 'forge/loggerConfig', version: DEFAULT_GRADLE)
-        gradle.gradleProperties << """
+	@Unroll
+	def "use fml logger config (minecraft #minecraft, forge #forge)"() {
+		setup:
+		def gradle = gradleProject(project: 'forge/loggerConfig', version: DEFAULT_GRADLE)
+		gradle.gradleProperties << """
 
         minecraft_version=$minecraft
         forge_version=$forge
         """.stripIndent()
 
-        when:
-        def result = gradle.run(task: 'generateLog4jConfig')
+		when:
+		def result = gradle.run(task: 'generateLog4jConfig')
 
-        then:
-        result.task(':generateLog4jConfig').outcome == SUCCESS
-        def logFile = new File(gradle.projectDir, '.gradle/loom-cache/log4j.xml')
-        logFile.text.contains('forge.logging')
+		then:
+		result.task(':generateLog4jConfig').outcome == SUCCESS
+		def logFile = new File(gradle.projectDir, '.gradle/loom-cache/log4j.xml')
+		logFile.text.contains('forge.logging')
 
-        where:
-        minecraft | forge
-        '1.19.4'  | '45.0.43'
-        '1.18.1'  | '39.0.63'
-        '1.17.1'  | '37.0.67'
-        '1.16.5'  | '36.2.4'
-        '1.14.4'  | '28.2.23'
-    }
+		where:
+		minecraft | forge
+		'1.19.4'  | '45.0.43'
+		'1.18.1'  | '39.0.63'
+		'1.17.1'  | '37.0.67'
+		'1.16.5'  | '36.2.4'
+		'1.14.4'  | '28.2.23'
+	}
 }
