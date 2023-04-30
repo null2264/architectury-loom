614136a1014e936f8c1280e90f860586767644cb
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-04-30 13:31:06 +0300

    Add test for single jar Forge

---
+++ src/test/groovy/net/fabricmc/loom/test/integration/forge/SingleJarTest.groovy
@@ -0,0 +1,65 @@
+/*
+ * This file is part of fabric-loom, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2023 FabricMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.fabricmc.loom.test.integration.forge
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import net.fabricmc.loom.test.util.CartesianProduct
+import net.fabricmc.loom.test.util.GradleProjectTestTrait
+
+import static net.fabricmc.loom.test.LoomTestConstants.STANDARD_TEST_VERSIONS
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+class SingleJarTest extends Specification implements GradleProjectTestTrait {
+	@Unroll
+	def "build single jar mc (mc #mc, forge #forge, env #env, gradle #version)"() {
+		setup:
+		def gradle = gradleProject(project: 'forge/singleJar', version: version)
+		gradle.buildGradle.text = gradle.buildGradle.text
+				.replace('@MCVERSION@', mc)
+				.replace('@FORGEVERSION@', forge)
+				.replace('@ENV@', env)
+
+		when:
+		def result = gradle.run(task: 'build')
+
+		then:
+		result.task(':build').outcome == SUCCESS
+
+		where:
+		[mc, forge, env, version] << CartesianProduct.addValuesToEach(
+		[
+			['1.19.4', "45.0.43"],
+			['1.18.1', "39.0.63"],
+			['1.17.1', "37.0.67"],
+			['1.16.5', "36.2.4"],
+			['1.14.4', "28.2.23"]
+		],
+		['client', 'server'],
+		STANDARD_TEST_VERSIONS
+		)
+	}
+}
---
+++ src/test/groovy/net/fabricmc/loom/test/util/CartesianProduct.groovy
@@ -0,0 +1,51 @@
+/*
+ * This file is part of fabric-loom, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2023 FabricMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.fabricmc.loom.test.util
+
+class CartesianProduct {
+	static <T> List<List<T>> addValuesToEach(List<? extends List<? extends T>> input, List<? extends T> values) {
+		List<List<T>> result = []
+
+		for (List<? extends T> row in input) {
+			for (T value in values) {
+				List<T> newRow = new ArrayList<>(row)
+				newRow.add(value)
+				result.add(newRow)
+			}
+		}
+
+		return result
+	}
+
+	static <T> List<List<T>> addValuesToEach(List<? extends List<? extends T>> input, List<? extends T>... values) {
+		List<List<T>> result = input as List<List<T>>
+
+		for (List<? extends T> valueList in values) {
+			result = addValuesToEach(result, valueList)
+		}
+
+		return result
+	}
+}
