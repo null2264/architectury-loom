e7cf25cdfd2a64b3c3de93b976aa01be9b0ba3f5
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-10-31 18:40:58 +0200

    Add test for basic NeoForge 1.20.2 projects

---
+++ src/test/groovy/net/fabricmc/loom/test/integration/neoforge/SimpleNeoForgeTest.groovy
@@ -0,0 +1,55 @@
+/*
+ * This file is part of fabric-loom, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2023 FabricMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.fabricmc.loom.test.integration.neoforge
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import net.fabricmc.loom.test.util.GradleProjectTestTrait
+
+import static net.fabricmc.loom.test.LoomTestConstants.DEFAULT_GRADLE
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+class SimpleNeoForgeTest extends Specification implements GradleProjectTestTrait {
+    @Unroll
+    def "build #mcVersion #neoforgeVersion #mappings"() {
+        setup:
+        def gradle = gradleProject(project: "neoforge/simple", version: DEFAULT_GRADLE)
+        gradle.buildGradle.text = gradle.buildGradle.text.replace('@MCVERSION@', mcVersion)
+                .replace('@NEOFORGEVERSION@', neoforgeVersion)
+                .replace('@MAPPINGS@', mappings)
+
+        when:
+        def result = gradle.run(task: "build")
+
+        then:
+        result.task(":build").outcome == SUCCESS
+
+        where:
+        mcVersion | neoforgeVersion | mappings
+        '1.20.2'  | '20.2.5-beta' | 'loom.officialMojangMappings()'
+        '1.20.2'  | '20.2.5-beta' | '"net.fabricmc:yarn:1.20.1+build.1:v2"'
+    }
+}
